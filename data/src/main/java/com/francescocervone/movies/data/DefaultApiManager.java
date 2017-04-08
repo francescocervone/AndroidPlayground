28a88a775e11283ef9c57017589b8f230b16b619
Francesco Cervone <cervonefrancesco@gmail.com>
2017-04-08 10:48:47 +0200

    added logging interceptor

--- data/src/main/java/com/francescocervone/movies/data/DefaultApiManager.java
+++ data/src/main/java/com/francescocervone/movies/data/DefaultApiManager.java
@@ -2,26 +2,35 @@ package com.francescocervone.movies.data;
 
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.francescocervone.movies.data.entities.MovieDetailsEntity;
 import com.francescocervone.movies.data.entities.MoviesPageEntity;
 
 import org.reactivestreams.Publisher;
 
+import java.io.IOException;
+
 import io.reactivex.Flowable;
 import io.reactivex.functions.Function;
 import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DefaultApiManager implements ApiManager {
 
-    public static final String BASE_URL = "https://api.themoviedb.org/3/";
-    public static final String QUERY_API_KEY = "api_key";
-    public static final String API_KEY = "594cf1f11605d5eecb156d4a7c22d1e5";
+    private static final String TAG = DefaultApiManager.class.getName();
+
+    private static final String BASE_URL = "https://api.themoviedb.org/3/";
+    private static final String API_KEY_QS = "api_key";
+    private static final String API_KEY_VALUE = "594cf1f11605d5eecb156d4a7c22d1e5";
+
     private final TheMovieDatabase mService;
 
     public DefaultApiManager() {
@@ -29,16 +38,9 @@ public class DefaultApiManager implements ApiManager {
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(new OkHttpClient.Builder()
-                        .addInterceptor(chain -> {
-                            HttpUrl url = chain.request().url()
-                                    .newBuilder()
-                                    .addQueryParameter(QUERY_API_KEY, API_KEY)
-                                    .build();
-                            Request request = chain.request().newBuilder()
-                                    .url(url)
-                                    .build();
-                            return chain.proceed(request);
-                        }).build())
+                        .addInterceptor(this::addApiKey)
+                        .addInterceptor(createLoggingInterceptor())
+                        .build())
                 .baseUrl(BASE_URL)
                 .build();
 
@@ -66,5 +68,30 @@ public class DefaultApiManager implements ApiManager {
     @NonNull
     private <T> Function<Throwable, Publisher<? extends T>> handleError() {
         return throwable -> Flowable.error(ErrorHandler.convert(throwable));
+    }
+
+    private Response addApiKey(Interceptor.Chain chain) throws IOException {
+
+        HttpUrl url = chain.request().url()
+                .newBuilder()
+                .addQueryParameter(API_KEY_QS, API_KEY_VALUE)
+                .build();
+
+        Request request = chain.request().newBuilder()
+                .url(url)
+                .build();
+
+        return chain.proceed(request);
+    }
+
+    @NonNull
+    private HttpLoggingInterceptor createLoggingInterceptor() {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(
+                message -> Log.d(TAG, message));
+
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+        return loggingInterceptor;
     }
 }
